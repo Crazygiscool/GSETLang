4039c6af46b6559c815dcf2ea2e41c7b55563252
crazygiscool <crazygiscool@proton.me>
2026-03-17 18:47:04 -0400

    Gotta import on the top

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
     // A sample input string with a header and code
     testInput := "shout=PRINT\nstop=EXIT\n---\nshout \"Hello from iSH!\""
     
     config, body := ParseGSet(testInput)
     
-    import "fmt" // Make sure to add "fmt" to your imports at the top!
+    // Make sure to add "fmt" to your imports at the top!
     fmt.Println("Keywords found:", config.Keywords)
     fmt.Println("Code body:", body)
 }
