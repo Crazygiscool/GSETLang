0e7c8c5c58906c6945d22e10c7c60558232d016d
Crazygiscool <crazygiscool@proton.me>
2026-04-17 21:53:16 -0400

    Added documentation and front page

--- docs/lib/layout.shared.tsx
+++ docs/lib/layout.shared.tsx
@@ -1,12 +1,20 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps, LinkItemType } from 'fumadocs-ui/layouts/shared';
 import { appName, gitConfig } from './shared';
 
 export function baseOptions(): BaseLayoutProps {
+  const links: LinkItemType[] = [
+    {
+      type: 'main',
+      url: '/docs',
+      text: 'Docs',
+    },
+  ];
+
   return {
     nav: {
-      // JSX supported
       title: appName,
     },
+    links,
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
 }
